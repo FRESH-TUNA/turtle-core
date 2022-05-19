4179c0d5fea17df6ba3ce5ce6bc6c81eaea4e60e
DONGWON KIM <lunacircle4@likelion.org>
2022-05-20 00:41:09 +0900

    feat: init commit

---
+++ src/test/java/com/remember/core/CoreApplicationTests.java
@@ -0,0 +1,15 @@
+package com.remember.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles(profiles = "test")
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
