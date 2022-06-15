cce65875a7b25813bae4492010bc4a638b3e50f8
DONGWON KIM <lunacircle4@likelion.org>
2022-06-15 14:09:15 +0900

    feat: 간단한 형태의 메인페이지 개발 #21

--- src/main/resources/db/seeds/algorithms.sql
+++ src/main/resources/db/seeds/algorithms.sql
@@ -1,3 +1,6 @@
 INSERT INTO `algorithm` (`name`) VALUES ('백트래킹 (backtracking)');
 INSERT INTO `algorithm` (`name`) VALUES ('문자열');
 INSERT INTO `algorithm` (`name`) VALUES ('정규표현식');
+
+INSERT INTO `algorithm` (`name`) VALUES ('조합');
+INSERT INTO `algorithm` (`name`) VALUES ('순열');
