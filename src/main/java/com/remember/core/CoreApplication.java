3ac0cac39a8ed0e891f5d4807680e5200abc1b55
DONGWON KIM <lunacircle4@likelion.org>
2022-06-22 15:58:47 +0900

    chore: 들여쓰기 수정

--- src/main/java/com/remember/core/CoreApplication.java
+++ src/main/java/com/remember/core/CoreApplication.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class CoreApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
 	}
-
 }
